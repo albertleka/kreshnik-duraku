e04b657a47e42eb1f90bf433921f2ae9088ba2da
Albert Lekaj <albertlekaj6@gmail.com>
2021-10-10 01:50:31 +0200

    header added
    Header added with menu and main content. Also font family applied.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily:{
+      sans:['Manrope', 'sans-serif']
+    },
+    extend: {
+
+    },
   },
   variants: {
     extend: {},
